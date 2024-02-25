95a820baa2662be4c364c04f0a1ad68ad11ee0ae
Asma <asmahashmi94@gmail.com>
2024-02-25 16:08:20 -0700

    page 2 changes

--- my-app/src/pages/signup.js
+++ my-app/src/pages/signup.js
@@ -24,32 +24,42 @@ function Signup() {
           </div>
 
           <div className='page2_inputfields'> 
-            <label for="businessname">Business Name:</label>
-            <input type="text" id="businessname" name="businessname"></input>
-            
-            <label for="phonenumber">Phone Number:</label>
-            <input type="text" id="phonenumber" name="phonenumber"></input>
-            
-            <label for="businessemail">Business Email:</label>
-            <input type="text" id="businessemail" name="businessemail"></input>
-            
-            <label for="password">Password:</label>
-            <input type="text" id="password" name="password"></input>
-           
-            <label for="businessaddress">Business Address:</label>
-            <input type="text" id="businessaddress" name="businessaddress"></input>
-  
-            <label for="businesscategory">Business Category</label>
-                <select id="businesscategory" name="businesscategory">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
+            <div className='businessname'> 
+                <label for="businessname">Business Name:</label>
+                <input type="text" id="businessname" name="businessname"></input>
+            </div>
+            <div className='businessname'> 
+                <label for="phonenumber">Phone Number:</label>
+                <input type="text" id="phonenumber" name="phonenumber"></input>
+            </div>
+            <div className='businessname'> 
+                <label for="businessemail">Business Email:</label>
+                <input type="text" id="businessemail" name="businessemail"></input>
+            </div>
+            <div className='businessname'> 
+                <label for="password">Password:</label>
+                <input type="text" id="password" name="password"></input>
+            </div>
+            <div className='businessname'> 
+                <label for="businessaddress">Business Address:</label>
+                <input type="text" id="businessaddress" name="businessaddress"></input>
+            </div>
+        
+            <div className='businessname'> 
+                <label for="businesscategory">Business Category</label>
+                    <select id="businesscategory" name="businesscategory">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="audi">Audi</option>
+                    </select>
+            </div>
           </div>
-          
-  
-          <button> Get Started</button>
+        <button> Get Started</button>
+        <div className='page2_account'>
+            <p>Already have an account? <a href="#">Sign In</a></p>
+        </div>
+
         </header>
       </div>
     );
